dbb1dc24880bbc2a05cf76b073c7b94d53e3a49b
kmoran04 <kristen.moran@tufts.edu>
2019-08-21 15:35:39 -0400

    Updates

---
+++ precache-manifest.a5cd7254e87f85b9d3fb0b5caf409ba4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00ba0cdd4567097b3c8ccbe4473e6526",
+    "url": "/index.html"
+  },
+  {
+    "revision": "67083c543f665420b6db",
+    "url": "/static/css/main.ffa29014.chunk.css"
+  },
+  {
+    "revision": "7cf9aa269cba94efd598",
+    "url": "/static/js/2.cff567ab.chunk.js"
+  },
+  {
+    "revision": "67083c543f665420b6db",
+    "url": "/static/js/main.3c80b937.chunk.js"
+  },
+  {
+    "revision": "e676c0768ba9136fc741",
+    "url": "/static/js/runtime~main.daaf4d4d.js"
+  }
+]);
